b237d0f0daa1073541c084871f0c18f8830a7d86
Andrew-Wand <andrewwands@gmail.com>
2024-03-27 20:34:49 -0500

    style: sign up page css for desktop and mobile

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -63,41 +63,93 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <div>
-        <Form onSubmit={handleLogin} ref={form}>
-          <div>
-            <label htmlFor="username">Username</label>
-            <Input
-              type="text"
-              name="username"
-              value={username}
-              onChange={onChangeUsername}
-              validations={[required]}
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <Input
-              type="password"
-              name="password"
-              value={password}
-              onChange={onChangePassword}
-              validations={[required]}
-            />
-          </div>
+    <div className="min-h-screen flex justify-center items-center bg-slate-400">
+      <div className="card w-96 bg-base-200 shadow-xl h-[30rem]">
+        <header className="mt-20">
+          <h1 className="card-title justify-center text-2xl">Log In</h1>
+        </header>
 
-          <div>
-            <button>
-              <span>Login</span>
-            </button>
-          </div>
+        <div className="card-body items-center justify-center text-center">
+          <div className="card-actions">
+            <div>
+              <Form onSubmit={handleLogin} ref={form}>
+                <div>
+                  <label htmlFor="username" className="text-xl">
+                    Username
+                  </label>
+                  <Input
+                    type="text"
+                    name="username"
+                    value={username}
+                    onChange={onChangeUsername}
+                    validations={[required]}
+                    className="input input-bordered bg-slate-100 shadow-lg mt-2"
+                  />
+                </div>
+                <div className="mt-3">
+                  <label htmlFor="password" className="text-xl">
+                    Password
+                  </label>
+                  <Input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={onChangePassword}
+                    validations={[required]}
+                    className="input input-bordered bg-slate-100 shadow-lg mt-2"
+                  />
+                </div>
 
-          {message && <div>{message}</div>}
-          <CheckButton ref={checkBtn} style={{ display: "none" }} />
-        </Form>
+                <div className="mt-5">
+                  <button className="btn btn-wide btn-info">
+                    <span>Login</span>
+                  </button>
+                </div>
+
+                {message && <div>{message}</div>}
+                <CheckButton ref={checkBtn} style={{ display: "none" }} />
+              </Form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+
+    // <div>
+    //   <div>
+    //     <Form onSubmit={handleLogin} ref={form}>
+    //       <div>
+    //         <label htmlFor="username">Username</label>
+    //         <Input
+    //           type="text"
+    //           name="username"
+    //           value={username}
+    //           onChange={onChangeUsername}
+    //           validations={[required]}
+    //         />
+    //       </div>
+    //       <div>
+    //         <label htmlFor="password">Password</label>
+    //         <Input
+    //           type="password"
+    //           name="password"
+    //           value={password}
+    //           onChange={onChangePassword}
+    //           validations={[required]}
+    //         />
+    //       </div>
+
+    //       <div>
+    //         <button>
+    //           <span>Login</span>
+    //         </button>
+    //       </div>
+
+    //       {message && <div>{message}</div>}
+    //       <CheckButton ref={checkBtn} style={{ display: "none" }} />
+    //     </Form>
+    //   </div>
+    // </div>
   );
 };
 
